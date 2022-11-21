464232dd4033fc5d875cfd508bf824f8a291de13
shenxh <shenxh0928@qq.com>
2022-11-21 22:10:17 +0800

    feat: 练习

--- src/views/demo.vue
+++ src/views/demo.vue
@@ -98,6 +98,95 @@
         </ul>
       </div>
     </div>
+
+    <!-- 条件渲染 -->
+    <div class="demo-item">
+      <h2>条件渲染</h2>
+      <div class="demo-item-content">
+        <div class="demo-item-content-box" @click="isShow = !isShow">
+          <span v-if="isShow">v-if 显示隐藏示例</span>
+        </div>
+        <div class="demo-item-content-box" @click="isShow1 = !isShow1">
+          <span v-show="isShow1">v-show 显示隐藏示例</span>
+        </div>
+        <ul class="demo-item-content-text">
+          <li>相同点:</li>
+          <li>v-if 和 v-show 都可以动态控制DOM元素的显示隐藏</li>
+          <li>不同点:</li>
+          <li>
+            <b>1. 编译方面：</b>
+            按控制手段和官方文献可知，v-if是惰性的，若最初指令值为false，它是不会编译的，直至指令值为true才开始局部编译存入缓存；v-show不管最初指令值为真假都会马上编译存入缓存，保留DOM
+          </li>
+          <li>
+            <b>2. 消耗方面：</b>
+            v-if 切换性能消耗较大；v-show 最初渲染消耗较大
+          </li>
+          <li>
+            <b>3. 适用场景：</b>
+            v-if 适用切换条件、项目需求稳定；v-show 适用频繁需要切换
+          </li>
+          <li>
+            <b>4. 语法方面：</b>
+            v-if 可与 v-else、 v-else-if
+            配合使用进行判断执行，但一定需要相邻，不可中断；v-show 无特殊语法
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <!-- 列表渲染 -->
+    <div class="demo-item">
+      <h2>列表渲染</h2>
+      <div class="demo-item-content">
+        <ul>
+          <li v-for="(item, index) in list" :key="index">
+            {{ item.name }}{{ index + 1 }}
+            {{ item.name + (index + 1) }}
+          </li>
+        </ul>
+
+        <ul>
+          <li
+            v-show="index % 2 === 0"
+            v-for="(item, index) in list1"
+            :key="index"
+          >
+            {{ item }}{{ index + 1 }}
+          </li>
+        </ul>
+        <ul class="demo-item-content-text">
+          <li>
+            不推荐同时使用 v-if 和 v-for
+          </li>
+          <li>
+            当 v-if 与 v-for 一起使用时，v-for 具有比 v-if 更高的优先级
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <!-- 表单输入绑定 -->
+    <div class="demo-item">
+      <h2>表单输入绑定</h2>
+      <div class="demo-item-content">
+        <div class="demo-item-content-test">
+          <input v-model="inputNum" type="number" /> +
+          <input v-model="inputNum1" type="number" /> = {{ inputTotal }}
+        </div>
+        <ul class="demo-item-content-text">
+          <li>
+            你可以用 v-model 指令在表单 input、textarea 及 select
+            元素上创建双向数据绑定。
+            <br />
+            它会根据控件类型自动选取正确的方法来更新元素。
+            <br />
+            尽管有些神奇，但 v-model 本质上不过是语法糖。
+            <br />
+            它负责监听用户的输入事件以更新数据，并对一些极端场景进行一些特殊处理。
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -120,12 +209,34 @@ export default {
       isActive: false,
       colorNum: 0,
       randomColor: [255, 255, 255],
+      isShow: true,
+      isShow1: true,
+      list: [
+        {
+          name: '红',
+        },
+        {
+          name: '绿',
+        },
+        {
+          name: '蓝',
+        },
+      ],
+      list1: ['一', '二', '三', '四', '五', '六', '七', '八', '九', '十'],
+      inputNum: '',
+      inputNum1: '',
     }
   },
   // 计算属性
   computed: {
     total() {
       return this.num + this.num1 + this.num2
+    },
+    inputTotal() {
+      let total = Number(this.inputNum) + Number(this.inputNum1)
+      let res = Number(total.toFixed(3))
+
+      return res
     },
     boxColor() {
       let color = ''
